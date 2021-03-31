d5f73b67655e0352db0ea9f248c1e9c973cf056d
Theo <theo17a@gmail.com>
2021-03-31 20:13:42 +0300

    Uplaoded my website

---
+++ canvas2.js
@@ -0,0 +1,130 @@
+// Multiple Animated Circle 
+var canvas = document.getElementById("canvas");
+// Set Canvas dimensions
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+// Set Canvas dimensions
+var c4 = canvas.getContext('2d');
+
+// The Circle class
+function Circle( x, y, dx, dy, radius ) {
+
+	this.x 	= x;
+	this.y 	= y;
+	this.dx = dx;
+	this.dy = dy;
+
+	this.radius = radius;
+
+	this.draw = function() {
+
+		c4.beginPath();
+		c4.arc( this.x, this.y,  this.radius, 0, Math.PI * 2, true  );
+		c4.strokeStyle = "#B5373F";
+        c4.lineWidth = 5;
+        c4.stroke();
+	}
+	
+	var mouseX=0;
+	var mouseY=0;
+	window.addEventListener('mousemove', function (e) {
+		mouseX=e.x;
+		mouseY=e.y;
+	});
+
+	this.update = function() {
+
+		if(Math.round(this.x) < mouseX+120 && Math.round(this.x) > mouseX-120 &&
+			Math.round(this.y) < mouseY+120 && Math.round(this.y) > mouseY-120){
+				var xDif=(Math.round(this.x)-mouseX);
+				var yDif=(Math.round(this.y)-mouseY);
+				
+				
+				if(xDif < 60 && xDif > 0){
+					this.dx+=0.2;
+				}
+				else if(xDif < 140 && xDif > 60){
+					this.dx+=0.1;
+				}	
+				else if(xDif > -60 && xDif < 0){
+					this.dx-=0.2;
+				}
+				else if(xDif > -140 && xDif < -60){
+					this.dx-=0.1;
+				}
+				if(yDif < 60 && yDif > 0){
+					this.dy+=0.2;
+				}
+				else if(yDif < 140 && yDif > 60){
+					this.dy+=0.1;
+				}	
+				else if(yDif > -60 && yDif < 0){
+					this.dy-=0.2;
+				}
+				else if(yDif > -140 && yDif < -60){
+					this.dy-=0.1;
+				}	
+		}
+		if(this.x + this.radius > 305 && this.x + this.radius < 500 || this.x - this.radius < 0 || this.x - this.radius < canvas.width-310 && this.x + this.radius > 500 || this.x + this.radius > canvas.width) {
+
+			this.dx = -this.dx;
+		}
+
+		if(this.y + this.radius > canvas.height || this.y - this.radius < 65) {
+
+			this.dy = -this.dy;
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;
+
+        this.draw();
+	}
+}
+
+var circles = [];
+var number = 9;
+
+for( var i = 0; i < number; i++ )  {
+
+	// Radius
+	var radius = 80;
+	var scaleSize=Math.random();
+	if(scaleSize>0.4){
+		radius=radius*scaleSize;
+	}
+	// Starting Position
+	if(i<4){
+	var x = Math.random() * 100 + radius;
+	var y = Math.random() * (canvas.height - radius * 2 - 65) + radius+65;
+	}
+	else{
+		var x = Math.random() * 100 + canvas.width - (100 + radius);
+		var y = Math.random() * (canvas.height - radius * 2 - 65) + radius+65;	
+	}	
+	// Speed in x and y direction
+	var dx = 0;
+	var xDirection = Math.random();
+	if(xDirection<0.5){dx = -1* Math.random() * 2.4;}
+  	else{dx = xDirection * 2.5;}
+  	var dy = Math.random() * 2.3;
+
+	circles.push( new Circle( x, y, dx, dy, radius ) );
+	
+}
+
+function animate() {
+  
+	requestAnimationFrame(animate);
+
+	c4.clearRect( 0, 0, canvas.width, canvas.height );
+
+	for( var r = 0; r < number; r++ ) {
+
+		circles[r].update();
+	}
+}
+
+animate();
+
+
